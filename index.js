0778c98275f847e7c18593dedb3d25b14bdb8b00
Roman Masliak <maslyak3@gmail.com>
2024-10-08 12:27:34 +0300

    feat: added score saving to localStorage

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-
-const gridSize = 20;
-canvas.width = 400;
-canvas.height = 400;
+let score = document.querySelector(".score");
+let scoreCount = 0;
+const gridSize = 10;
+canvas.width = 200;
+canvas.height = 200;
 const squareAmount = canvas.width / gridSize;
+let previousResults = document.querySelector('.previous-results');
+let stringResults = localStorage.getItem('statistic');
+let results;
+
+
+if (stringResults) {
+    results = JSON.parse(stringResults);
+} else {
+    results = [];
+};
+console.log(results)
+const foodAudio = new Audio('src/food.mp3');
+const moveAudio = new Audio('src/move.mp3');
+const endGameAudio = new Audio('src/gameover.mp3');
+const winAudio = new Audio('src/win.mp3');
 
 let snake = [{ x: 10, y: 10 }];
 let moveDirection = { x: 1, y: 0 };
 let item = { x: 5, y: 5 };
 
+let startOrNot = false;
+document.addEventListener('keydown', (interaction) => {
+    startOrNot = true;
+    });
+
 function game() {
-    progress();
-    drawSnake();
+    if (startOrNot) {
+        progress();
+        drawSnake();
+    }
 }
 
 function progress() {
@@ -20,7 +43,16 @@ function progress() {
         x: snake[0].x + moveDirection.x,
         y: snake[0].y + moveDirection.y
     }
-    console.log(newHead);
+    if (
+      newHead.x < 0 ||
+      newHead.x > squareAmount ||
+      newHead.y < 0 ||
+      newHead.y > squareAmount
+    ) {
+        endGame();
+        endGameAudio.play();
+      return;
+    }
 
    snake.unshift(newHead);
     if (
@@ -29,6 +61,19 @@ function progress() {
             x: Math.floor(Math.random() * squareAmount),
             y: Math.floor(Math.random() * squareAmount)
         }
+
+        scoreCount++
+        if (scoreCount === 10) {
+            score.innerHTML = `You win, your score: ${scoreCount}`;
+            clearTimeout(interval);
+            winAudio.play();
+
+        }
+                else {
+            score.innerHTML = (`Your score: ${scoreCount}`)
+        }
+        foodAudio.play()
+
         drawFood();
     }
     else {
@@ -49,6 +94,7 @@ function drawSnake() {
         );
         drawFood();
     });
+
 }
 
 function drawFood() {
@@ -61,33 +107,41 @@ function drawFood() {
     );
 }
 
-// function endGame() {
-//     snake = [{ x: 10, y: 10 }];
-//     moveDirection = [{ x: 1, y: 0 }];
-//     item = [{ x: 5, y: 5 }];
+function endGame() {
+    snake = [{ x: 10, y: 10 }];
+    moveDirection = [{ x: 1, y: 0 }];
+    item = [{ x: 5, y: 5 }];
+    score.innerHTML = `You failed with the score ${scoreCount}`;
+    results.unshift(scoreCount);
 
-// }
+    localStorage.setItem('statistic', JSON.stringify(results));
+}
+previousResults.innerHTML = `Previous results: <br>${results[0]} <br>${results[1]}<br>${results[2]}<br>${results[3]}<br>${results[4]}<br>${results[5]}<br>${results[6]}<br>${results[7]}<br>${results[8]}<br>${results[9]}`;
 
 window.addEventListener('keydown', action => {
     clearInterval(interval);
 
     switch (action.key) {
         case 'ArrowUp':
+            moveAudio.play()
             if (moveDirection.y === 0) moveDirection = { x: 0, y: -1 };
             break;
         case 'ArrowDown':
+            moveAudio.play();
             if (moveDirection.y === 0) moveDirection = { x: 0, y: 1 };
             break;
         case 'ArrowLeft':
+            moveAudio.play();
             if (moveDirection.x === 0) moveDirection = { x: -1, y: 0 };
             break;
         case 'ArrowRight':
+            moveAudio.play();
             if (moveDirection.x === 0) moveDirection = { x: 1, y: 0 };
             break;
     }
     progress();
-    interval = setInterval(game, 500);
+    interval = setInterval(game, 300);
 });
 
 drawFood();
-let interval = setInterval(game, 500);
+let interval = setInterval(game, 300);
